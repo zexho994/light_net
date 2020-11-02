7b03ccda8a94418ebc4da85a58fff86a6810675a
zouzhihao-994 <zouzhihao994@gmail.com>
2020-11-02 22:56:20 +0800

    add socket client & server

--- src/socket/client/client.go
+++ src/socket/client/client.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"sync"
 )
 
@@ -22,8 +24,36 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Connecting to " + *host + ":" + *port)
 
-	// 使用waitgroup进行同步
+	// 使用wait group进行同步
 	var wg sync.WaitGroup
 	wg.Add(2)
 
+	go handleWrite(conn, &wg)
+	go handleRead(conn, &wg)
+
+	wg.Wait()
+}
+
+func handleWrite(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 10; i > 0; i-- {
+		_, e := conn.Write([]byte("hello " + strconv.Itoa(i) + "\r\n"))
+		if e != nil {
+			fmt.Println("Error to send message because of ", e.Error())
+			break
+		}
+	}
+}
+
+func handleRead(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	reader := bufio.NewReader(conn)
+	for i := 1; i <= 10; i++ {
+		line, err := reader.ReadString(byte('\n'))
+		if err != nil {
+			fmt.Print("Error to read message because of ", err)
+			return
+		}
+		fmt.Print(line)
+	}
 }
--- src/socket/server/server.go
+++ src/socket/server/server.go
@@ -8,16 +8,16 @@ import (
 	"os"
 )
 
-var host = flag.String("host", "localhost", "listening host , ip4 or ip6 or localhost")
-var port = flag.String("port", "9944", "listening port , 5000 ~ 65535")
-var pf = flag.String("pf", "tcp", "<tcp> or <udp> or <all>")
+var host = flag.String("host", "localhost", "host, ip4 or ip6 or localhost")
+var port = flag.String("port", "9944", "port 5000 ~ 65535")
+var pf = flag.String("pf", "tcp", "protocol family, <tcp> or <udp>")
 
 // 创建socket
 func main() {
 	flag.Parse()
 	listen, err := net.Listen(*pf, *host+":"+*port)
 	if err != nil {
-		panic("Error listening")
+		fmt.Println("Error listening", err)
 		os.Exit(1)
 	}
 
@@ -28,11 +28,11 @@ func main() {
 
 		conn, err := listen.Accept()
 		if err != nil {
-			panic("Error accepting")
+			fmt.Println("Error Accept,", err)
 			os.Exit(1)
 		}
 
-		fmt.Println("Received message %s -> %s \n", conn.RemoteAddr(), conn.LocalAddr())
+		fmt.Printf("Received message %s -> %s \n", conn.RemoteAddr(), conn.LocalAddr())
 
 		go handleRequest(conn)
 	}
